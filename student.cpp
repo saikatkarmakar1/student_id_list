c93a532abe3fd36c9876bcee3182227e2f7a7aff
SAIKAT KARMAKAR <saikat@saikat.indinet.co.in>
2026-03-28 17:05:36 +0530

    done user input system

---
+++ student.cpp
@@ -0,0 +1,67 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Student {
+private:
+public:
+  void take_inp(void) {
+    int roll, mob_no, perse;
+    string fast_name, last_name, address, mobile_no;
+    while (true) {
+      cout << "Enter student roll no : ";
+      cin >> roll;
+      string _roll_ = to_string(roll);
+      if (_roll_.length() == 0) {
+        cout << "please enter Roll no" << endl;
+      } else {
+        break;
+      }
+    }
+    cout << "Enter student name : ";
+    cin >> fast_name >> last_name;
+    while (true) {
+      cout << "Enter Your mobile no (10 digit) : ";
+      cin >> mob_no;
+      mobile_no = to_string(mob_no);
+
+      if (mobile_no.length() == 10) {
+        break;
+      } else {
+        cout << "Invalid! mobile no , Enter again . " << endl;
+      }
+    }
+    while (true) {
+      cout << "Enter address : ";
+      cin >> address;
+      if (address.length() == 0) {
+        cout << "Please enter address . ";
+      } else {
+        break;
+      }
+    }
+    string roll_str = to_string(roll);
+    string total = "\nRoll no : " + roll_str + "\nName : " + fast_name + " " +
+                   last_name + "\nMobile No. : " + mobile_no +
+                   "\nAdress : " + address + "\n";
+    cout << total << endl;
+    ofstream file("list.txt", ios::app);
+    if (!file) {
+      cout << "File no open ," << endl;
+      return;
+    }
+    file << total;
+    file.close();
+    cout << "Student add completeddd... ;) " << endl;
+  }
+  void cheak_id(void) {}
+};
+
+int main() {
+  Student saikat;
+  saikat.take_inp();
+
+  return 0;
+}
