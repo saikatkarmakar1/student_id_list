f64955e293339548a1109a03b38de6a9ba9e7708
SAIKAT KARMAKAR <saikat@saikat.indinet.co.in>
2026-03-28 19:48:37 +0530

    show user solv

--- student.cpp
+++ student.cpp
@@ -62,31 +62,56 @@ public:
     cout << "Student add completeddd... ;) " << endl;
   }
   void cheak_id(void) {
+    student s;
     int id;
-    cout << "Enter id to find student (If you show all student list then put 0 "
-            "): ";
+    bool fount = false;
+    // while (true) {
+    cout << "Enter ID that user you can cheak : ";
     cin >> id;
-    if (id == 0) { // That is for cheal all id ;
-      cout << "That is all user list :--- " << endl;
-      ifstream file("list.txt");
-      string line;
 
-      if (!file) {
-        cout << "File not open ." << endl;
-        return;
-      }
-
-      while (getline(file, line)) {
-        cout << line << endl;
-      }
-      file.close();
+    // if (id == 0) {
+    //  cout << "ID cannot be 0 . Enter write id ." << endl;
+    //} else {
+    FILE *file = fopen("student.dat", "rb");
+    if (!file) {
+      cout << "No file found ." << endl;
+      return;
     }
+    while (fread(&s, sizeof(student), 1, file)) {
+      if (s.id == id) {
+        cout << "\n--- Student Found ---\n";
+        cout << "ID      : " << s.id << "\n";
+        cout << "Name    : " << s.name << "\n";
+        cout << "Mobile  : " << s.mobile_no << "\n";
+        cout << "Address : " << s.address << "\n";
+        fount = true;
+        break;
+      }
+    }
+    fclose(file);
+    //}
+    //}
   }
 };
 
 int main() {
+  int option;
   Student saikat;
-  saikat.take_inp();
+  while (true) {
 
+    cout << "Student Page : \n1) Add user \n2) Cheak student\nEnter Your "
+            "option : ";
+    cin >> option;
+
+    if (option == 1) {
+      saikat.take_inp();
+      break;
+    } else if (option == 2) {
+      saikat.cheak_id();
+      break;
+    } else {
+      cout << "You put wrong option . " << endl;
+    }
+  }
   return 0;
 }
